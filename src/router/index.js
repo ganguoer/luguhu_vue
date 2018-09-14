2b213cef5c9281ca76a006e94223bc72282effaa
ganguoer <ganguoer_dan@163.com>
2018-09-14 17:49:19 +0800

    code

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,14 @@ import charm from '@/components/charm/charm'  //魅力泸沽湖
 import charmVR from '@/components/charm/charmVR'
 
 import tourism from '@/components/tourism/tourism'  //旅游攻略
+import tourism_s from '@/components/tourism/tourism_s'  //旅游攻略二级页面
+import eating_s from '@/components/tourism/eating_s'  //吃 二级页面
+import shopping_s from '@/components/tourism/shopping_s'  //购物 二级页面
+import line_s from '@/components/tourism/line_s'  //线路 二级页面
+import travel_s from '@/components/tourism/travel_s'  //旅游 二级页面
+
+import scenic_s from '@/components/scenicIntroduction/scenic_s'  //景区介绍二级页面
+
 
 Vue.use(Router)
 
@@ -17,7 +25,7 @@ export default new Router({
       component: Index
     },
     {
-      path: '/charm',
+      path: '/charm',   //魅力泸沽湖
       name: 'charm',
       component: charm
     },
@@ -27,9 +35,39 @@ export default new Router({
       component: charmVR
     },
     {
-      path: '/tourism',
+      path: '/tourism',  //旅游攻略
       name: 'tourism',
       component: tourism
+    },
+    {
+      path: '/tourism_s',
+      name: 'tourism_s',
+      component: tourism_s
+    },
+    {
+      path: '/eating_s',
+      name: 'eating_s',
+      component: eating_s
+    },
+    {
+      path: '/shopping_s',
+      name: 'shopping_s',
+      component: shopping_s
+    },
+    {
+      path: '/line_s',
+      name: 'line_s',
+      component: line_s
+    },
+    {
+      path: '/travel_s',
+      name: 'travel_s',
+      component: travel_s
+    },
+    {
+      path: '/scenic_s',  //景区介绍
+      name: 'scenic_s',
+      component: scenic_s
     },
     // {
     //   path: '/footer',
