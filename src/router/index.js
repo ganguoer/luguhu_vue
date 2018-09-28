6c8ced0eb6cd5d423f6dd066182528e0e328adce
ganguoer <ganguoer_dan@163.com>
2018-09-28 15:18:10 +0800

    code1

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,17 @@ import charmVR from '@/components/charm/charmVR'
 
 import tourism from '@/components/tourism/tourism'  //旅游攻略
 import tourism_s from '@/components/tourism/tourism_s'  //旅游攻略二级页面
+import eating from '@/components/tourism/eating'  //吃 一级页面
 import eating_s from '@/components/tourism/eating_s'  //吃 二级页面
+import shopping from '@/components/tourism/shopping'  //购物 一级页面
 import shopping_s from '@/components/tourism/shopping_s'  //购物 二级页面
 import line_s from '@/components/tourism/line_s'  //线路 二级页面
 import travel_s from '@/components/tourism/travel_s'  //旅游 二级页面
+import walk_s from '@/components/tourism/walk_s'  //旅游 二级页面
+import living from '@/components/tourism/living'  //住 一级页面
+import living_s from '@/components/tourism/living_s'  //住 二级页面
 
+import scenic from '@/components/scenicIntroduction/scenic'  //景区介绍页面
 import scenic_s from '@/components/scenicIntroduction/scenic_s'  //景区介绍二级页面
 
 
@@ -45,9 +51,19 @@ export default new Router({
       component: tourism_s
     },
     {
+      path: '/eating',
+      name: 'eating',
+      component: eating
+    },
+    {
       path: '/eating_s',
       name: 'eating_s',
       component: eating_s
+    },
+    {
+      path: '/shopping',
+      name: 'shopping',
+      component: shopping
     },
     {
       path: '/shopping_s',
@@ -65,14 +81,30 @@ export default new Router({
       component: travel_s
     },
     {
-      path: '/scenic_s',  //景区介绍
+      path: '/walk_s',
+      name: 'walk_s',
+      component: walk_s
+    },
+    {
+      path: '/living',
+      name: 'living',
+      component: living
+    },
+    {
+      path: '/living_s',
+      name: 'living_s',
+      component: living_s
+    },
+    {
+      path: '/scenic',  //景区介绍
+      name: 'scenic',
+      component: scenic
+    },
+    {
+      path: '/scenic_s',  //景区介绍二级页面
       name: 'scenic_s',
       component: scenic_s
-    },
-    // {
-    //   path: '/footer',
-    //   name: 'footer',
-    //   component: footer
-    // }
+    }
+
   ]
 })
